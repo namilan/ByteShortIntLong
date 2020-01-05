43d4a2522909ceb7aaad699f5db22473f93d7380
namilan <nichole.milan@gmail.com>
2020-01-04 19:35:55 -0500

    practice

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,27 @@ public class Main {
 		
 		System.out.println("Integer Minimum Value = " + myMinIntValue);
 		System.out.println("Integer Maximum Value = " + myMaxIntValue);
+		System.out.println("Busted MAX value =  " + (myMaxIntValue + 1));
+		System.out.println("Busted MIN value = " + (myMinIntValue - 1));
+		
+		int myMaxIntTest = 2147483647;
+		
+		byte myMinByteValue = Byte.MIN_VALUE;
+		byte myMaxByteValue = Byte.MAX_VALUE;
+		System.out.println("Byte Minimum Value = " + myMinByteValue);
+		System.out.println("Byte Maximum Value = " + myMaxByteValue);
+		
+		short myMinShortValue = Short.MIN_VALUE;
+		short myMaxShortValue = Short.MAX_VALUE;
+		System.out.println("Short Minimum Value = " + myMinShortValue);
+		System.out.println("Short Maximlum Value = " + myMaxShortValue);
+		
+		long myLongValue = 100L;
+		
+		long myMinLongValue = Long.MIN_VALUE;
+		long myMaxLongValue = Long.MAX_VALUE;
+		System.out.println("Long Minimum Value = " + myMinLongValue);
+		System.out.println("Long Maximum Value = " + myMaxLongValue);
 	}
 
 }
